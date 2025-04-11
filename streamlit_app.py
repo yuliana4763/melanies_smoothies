44a670069e75031fa46d6f61b9bb293379bd925b
Yuliana <43132643+yuliana4763@users.noreply.github.com>
2025-04-11 16:46:47 +0700

    Create streamlit_app.py
    copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,41 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
+st.write(
+  """Choose the fruits you want in your custom Smoothie!.
+  """)
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections=5
+)
+
+if ingredients_list and name_on_order:  # Pastikan ada bahan dan nama
+    # Gabungkan daftar bahan menjadi string dengan koma sebagai pemisah
+    ingredients_string = ', '.join(ingredients_list)
+
+    # Tombol untuk mengonfirmasi pesanan
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        # Gunakan parameterized query agar lebih aman
+        session.sql(
+            "INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES (?, ?)", 
+            [ingredients_string, name_on_order]
+        ).collect()
+
+        st.success('Your Smoothie is ordered!', icon="✅")
+
+
